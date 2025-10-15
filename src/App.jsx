3359b1c6e8f503a6a704b7c15e2c00b32ee0698a
thaventhiran Ramsan <ramsandota@gmail.com>
2025-10-15 13:48:25 +0530

    First inisizization

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,44 @@
 import "./App.css";
 
+function Header() {
+  return (
+    <>
+      <div>
+        <h1> Head-React</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit obcaecati iste provident, quidem distinctio veritatis accusamus magni soluta dolor. Maxime rem quis quae impedit rerum recusandae modi, iure officiis suscipit!</p>
+      </div>
+    </>
+  );
+}
+function Welcome() {
+  return (
+    <>
+      <div>
+        <h1> Welcome-React</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor voluptatibus autem perspiciatis, accusantium voluptatem qui cum, atque quidem voluptates quae, est facere ipsum illum natus similique exercitationem perferendis. Et, culpa.</p>
+      </div>
+    </>
+  );
+}
+function Footer() {
+  return (
+    <>
+      <div>
+        <h1> Footer-React </h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. A, voluptas! Amet impedit temporibus dolores accusamus necessitatibus quidem sunt modi id laboriosam, voluptatum aliquid est totam maxime tempore quam velit quisquam.</p>
+      </div>
+    </>
+  );
+}
+
 function App() {
   return (
     <>
       <div>
-        <h1> React-setup</h1>
+         <Header />
+        <Welcome />
+        <Footer />
+       
       </div>
     </>
   );
